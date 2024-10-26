2f890b90d10adfb06306e80c9ac5e7dc03e59067
Nikolay <103648794+gilotin@users.noreply.github.com>
2024-10-26 16:37:39 +0300

    feat: added desktop layout

--- script.js
+++ script.js
@@ -13,16 +13,16 @@ form.addEventListener("input", (e) => {
 
     if (!emailPattern.test(inputField.value)) {
         errorSign.style.display = "block";
-        errorLabel.style.display = "block";
+        errorLabel.classList.add("active");
         inputField.classList.add("error");
     } else {
         errorSign.style.display = "none";
-        errorLabel.style.display = "none";
+        errorLabel.classList.remove("active");
     }
 
     if (inputField.value == "") {
         errorSign.style.display = "none";
-        errorLabel.style.display = "none";
+        errorLabel.classList.remove("active");
         inputField.classList.remove("error");
     }
 });
